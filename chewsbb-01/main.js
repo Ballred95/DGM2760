f4a72e44b7bb8e8b3b52912bd912ff20e9e83423
Ballred95 <blake.allred95@icloud.com>
2020-08-25 19:58:07 -0600

    WOK

--- chewsbb-01/main.js
+++ chewsbb-01/main.js
@@ -1,11 +1,17 @@
-document.querySelector('#company').innerHTML='Chewys bed and breakfast'
-document.querySelector('#slogan').innerHTML = 'from a galaxy far far away'
-
 const userName = prompt('enter name')
 console.log(userName)
 
-function myFunc (userName) {
-   return alert(`hello ${userName}`)
-}
 
-window.onload = myFunc(userName)
+document.querySelector('#company').innerHTML='Chewys bed and breakfast'
+document.querySelector('#slogan').innerHTML = 'from a galaxy far far away'
+document.querySelector('.popup').innerHTML=`Hello ${userName}, I am C3P0 Human Cyborg Relations!`
+
+
+
+// function myFunc (userName) {
+//    return alert(`hello ${userName}`)
+// }
+
+// window.onload = myFunc(userName)
+
+
